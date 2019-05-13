ada3613fc3a75deef3cebaddfc96a0c2114770a1
markovchainy <markovchainy@users.noreply.github.com>
2019-05-13 19:28:21 +0100

    Use transparent comparison in map::erase

--- blox/map.cpp
+++ blox/map.cpp
@@ -3,14 +3,17 @@
 namespace blox {
 
 std::pair<iterator, bool> map::insert(datastore::value_type value) {
-  auto result = data_.insert(std::pair(std::string(value.first),
-                                       std::string(value.second)));
+  auto pair = std::pair(std::string(value.first), std::string(value.second));
+  auto result = data_.insert(pair);
   auto it = iterator(std::make_unique<cursor>(result.first));
   return std::pair(std::move(it), result.second);
 }
 
 datastore::size_type map::erase(datastore::key_type key) {
-  return data_.erase(std::string(key));
+  auto it = data_.find(key);
+  auto result = it == data_.end() ? 0 : 1u;
+  data_.erase(it);
+  return result;
 }
 
 iterator map::find(datastore::key_type key) const {
@@ -25,28 +28,18 @@ iterator map::end() const {
   return iterator(std::make_unique<cursor>(data_.end()));
 }
 
-map::cursor::cursor(map::cursor::iterator it)
-    : it_(it) {
-}
+map::cursor::cursor(map::cursor::iterator it) : it_(it) {}
 
-std::string_view map::cursor::key() const {
-  return std::string_view();
-}
+std::string_view map::cursor::key() const { return std::string_view(); }
 
-std::string_view map::cursor::value() const {
-  return std::string_view();
-}
+std::string_view map::cursor::value() const { return std::string_view(); }
 
 bool map::cursor::equal(const blox::cursor& rhs) const {
   auto c = dynamic_cast<const map::cursor&>(rhs);
   return it_ == c.it_;
 }
 
-void map::cursor::increment() {
-  ++it_;
-}
+void map::cursor::increment() { ++it_; }
 
-void map::cursor::decrement() {
-  --it_;
-}
-}
+void map::cursor::decrement() { --it_; }
+}  // namespace blox
