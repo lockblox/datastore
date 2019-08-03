b2234dc318156f6afc33cefcfb069ea119478427
markovchainy <markovchainy@users.noreply.github.com>
2019-08-03 13:59:18 +0100

    Corrected formatting

--- test/block_test.cpp
+++ test/block_test.cpp
@@ -5,22 +5,21 @@ namespace test {
 
 TEST(block, equal) {
   auto a = blox::block("some data");
-    auto b = blox::block("some other data");
-    EXPECT_NE(a, b);
-    b = blox::block(a.key(), b.data()); // make keys match
-    EXPECT_EQ(a,b);
-    b = blox::block(a.key()); // no data in block b
-    EXPECT_EQ(a,b);
-    auto a_copy = a;
-    EXPECT_EQ(a_copy.key(), a.key());
-    EXPECT_EQ(a_copy.data(), a.data());
-    EXPECT_EQ(a, b);
-    a = a;
-    EXPECT_EQ(a,a);
-    blox::block c{b};
-    EXPECT_EQ(b, c);
-    auto a_move = std::move(a);
-    EXPECT_EQ(a_move, a_copy);
+  auto b = blox::block("some other data");
+  EXPECT_NE(a, b);
+  b = blox::block(a.key(), b.data());  // make keys match
+  EXPECT_EQ(a, b);
+  b = blox::block(a.key());  // no data in block b
+  EXPECT_EQ(a, b);
+  auto a_copy = a;
+  EXPECT_EQ(a_copy.key(), a.key());
+  EXPECT_EQ(a_copy.data(), a.data());
+  EXPECT_EQ(a, b);
+  EXPECT_EQ(a, a);
+  blox::block c{b};
+  EXPECT_EQ(b, c);
+  auto a_move = std::move(a);
+  EXPECT_EQ(a_move, a_copy);
 }
 
 TEST(block, empty) {
@@ -35,4 +34,4 @@ TEST(block, set) {
   EXPECT_EQ(4u, block_set.size());
 }
 
-}
+}  // namespace test
